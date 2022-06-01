cd2c751c0f97724d43cfb3c9172cc173fd7ef0c8
kaetojn <kaetondu@gmail.com>
2022-05-31 20:34:05 -0400

    update server.js

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
+// app.all('*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type')
+//   next()
+// })
+
+app.options('*', cors())
 app.use(cookieParser())
 app.use(express.json())
 
